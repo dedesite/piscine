cc14d2a8df44aaa084f5f4c72dd3a0305afd698b
Andréas Livet <andreas.livet@gmail.com>
2013-11-27 15:33:12 +0100

    Ajout de prototype de fonction de check

--- evalexpr/main.c
+++ evalexpr/main.c
@@ -1,6 +1,21 @@
 #include "evalexpr.h"
 #include "my.h"
 
+int check_base(char* b)
+{
+
+}
+
+int check_ops(char* ops)
+{
+
+}
+
+char* get_expr(unsigned int size)
+{
+
+}
+
 int main(int ac, char **av)
 {
     if (ac > 1)
