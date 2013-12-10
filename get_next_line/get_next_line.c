dcf916d1012345963fe4e4a94a0ee80be8fe18c4
Andréas Livet <andreas.livet@gmail.com>
2013-12-10 09:27:28 +0100

    Suppression du legacy code

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -20,98 +20,6 @@ char* my_realloc(char* ptr, size_t size)
     return (ptr);
 }
 
-/*
-char *get_next_line(const int fd)
-{
-    static char *buff = 0;
-    static int len = 0;
-    int i;
-    char* str;
-
-    if (!buff)
-        buff = malloc(BUFFER_SIZE * sizeof(char));
-    if (!*buff)
-        len = read(fd, buff, BUFFER_SIZE);
-    if (len == 0)
-        return (0);
-    i = 0;
-    str = malloc(len + 1);
-    while (i < len && buff[i] != '\n')
-    {
-        str[i] = buff[i];
-        i++;
-    }
-    str[i] = 0;
-    if (i >= len - 1)
-    {
-        //Prob : buff n'est pas forcément à BUFFER_SIZE
-        printf("here");
-        //buff = buff - BUFFER_SIZE - 1 + (i == len);
-        //buff[0] = 0;
-        if(i == len)
-        {
-            printf("ligne longue");
-        }
-    }
-    else
-    {
-        printf("here");
-        buff = &buff[i + 1];
-        len = len - (i + 1);
-        printf("len:%d\n",len );
-    }
-    return (str);
-}
-*/
-/*
-char *get_next_line(const int fd)
-{
-    static int prev_ind = 0;
-    static int prev_len = 0;
-    static char buff[BUFFER_SIZE];
-    int len;
-    int i;
-    int j;
-    char* str;
-
-    if(prev_ind == 0)
-    {
-        len = read(fd, buff, BUFFER_SIZE);
-        prev_len = len;
-        printf("read:%d\n", len);
-    }
-    else
-    {
-        len = prev_len - prev_ind;
-        //printf("keep:%d\n", len);
-    }
-    str = malloc(len + 1);
-    if(str == NULL || len == 0)
-        return (0);
-    i = prev_ind;
-    while(i < prev_ind + len && buff[i] != '\n')
-        i++;
-    len = i - prev_ind;
-    //printf("new len:%d\n", len);
-    
-    i = 0;
-    j = prev_ind;
-    while(i < len)
-    {
-        //printf("buff=%s\n", &buff[j]);
-        str[i] = buff[j];
-        i++;
-        j++;
-    }
-    str[i] = 0;
-    printf("%s\n", str);
-    if(j != prev_len)
-        prev_ind = j + 1;
-    else
-        prev_ind = 0;
-    return (str);
-}*/
-
 char get_char(const int fd)
 {
     static char buff[BUFFER_SIZE];
@@ -131,7 +39,6 @@ char get_char(const int fd)
     len--;
     return c;
 }
-
 
 char *get_next_line(const int fd)
 {
